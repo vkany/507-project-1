309cc91efca4affd8bbbd9ca35ffbae84a66093a
Vibhuti Kanitkar <vibhuti@umich.edu>
2017-09-14 15:47:17 -0400

    added card testfor class card

--- SI507F17_project1_tests.py
+++ SI507F17_project1_tests.py
@@ -9,3 +9,36 @@ from SI507F17_project1_cards import *
 ## You should invoke the tests with verbosity=2 (make sure you invoke them!)
 
 ###########
+class CardTest(unittest.TestCase):
+
+    def setUp(self):
+        self.card = Card(2, 13)
+
+    def test1(self): # #checking correst suit_names
+        # self.assertEqual(self.card.rank_num, 1, 'check values')
+        self.assertEqual(type(self.card.suit_names),list)
+
+    def test2(self):
+        self.assertEqual(self.card.suit,'Hearts', 'check value of ')
+
+    def test3(self):
+        self.assertEqual(self.card.rank,"King")
+
+    def test4(self):
+        self.assertEqual(type(self.card.rank_num),int)
+
+    def test5(self):
+        self.assertEqual(str(self.card), 'King of Hearts') #The Card class has a string method, which should return a string e.g. "Ace of Spades" or "3 of Clubs", etc.
+
+class DeckTest(unittest.TestCase):
+
+    def setUp(self):
+        self.d = Deck()
+
+    def test6(self):
+        self.assertEqual(type(self.d.cards), list)
+
+    # def test8(self):
+
+
+unittest.main(verbosity=2)
